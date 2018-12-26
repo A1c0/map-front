816bee173839708c6347d3b4d382bd5a05b56c15
Alexia COUTON <alex.couton@gmail.com>
2018-12-26 21:58:24 +0100

    update legends

--- src/app/legends/legends.component.ts
+++ src/app/legends/legends.component.ts
@@ -10,18 +10,24 @@ export class LegendsComponent implements OnInit {
 
   private villains;
   private villainsByCities;
+  private heroes;
 
   constructor(private http: HttpClient) {
     this.villains = null;
     this.villainsByCities = null;
+    this.heroes = null;
   }
 
   async ngOnInit() {
     await this.getVillains();
     await this.getVillainsByCity();
+    await this.getHeroes();
     setInterval(() => {
       this.getVillains();
       this.getVillainsByCity();
+      this.getHeroes();
+      console.log('hero :');
+      console.log(this.heroes);
     }, 1000);
   }
 
@@ -30,7 +36,6 @@ export class LegendsComponent implements OnInit {
     this.villains = await new Promise((resolve) => {
       res.subscribe(resolve);
     });
-    console.log(this.villains);
   }
 
   async getVillainsByCity() {
@@ -38,6 +43,13 @@ export class LegendsComponent implements OnInit {
     this.villainsByCities = await new Promise((resolve) => {
       res.subscribe(resolve);
     });
-    console.log(this.villainsByCities);
   }
+
+  async getHeroes() {
+    const res = this.http.get('http://0.0.0.0:3070/getHeroesFront');
+    this.heroes = await new Promise((resolve) => {
+      res.subscribe(resolve);
+    });
+  }
+
 }
